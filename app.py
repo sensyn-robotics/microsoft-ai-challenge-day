40c75c80c84f626089d2092b0d74b97ee5d839da
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-06-12 05:27:43 +0900

    print version

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 # This is the base file for RAG based on: 
 # ref: https://github.com/shohei1029/book-azureopenai-sample/blob/main/aoai-rag/notebooks/02_RAG_AzureAISearch_PythonSDK.ipynb
 
+import azure.search.documents
+print(f"azure search version={azure.search.documents.__version__}")
