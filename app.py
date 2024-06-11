a61e1a67c9887ab8566086463e8da8cc8de1f94a
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-06-12 05:47:14 +0900

    create azure openai

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # This is the base file for RAG based on:
 # ref: https://github.com/shohei1029/book-azureopenai-sample/blob/main/aoai-rag/notebooks/02_RAG_AzureAISearch_PythonSDK.ipynb
 
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+from openai.types.chat import (
+    ChatCompletion,
+    ChatCompletionChunk,
+)
+from openai import AzureOpenAI
 from azure.search.documents.indexes import SearchIndexClient
 from azure.search.documents import SearchClient, SearchIndexingBufferedSender
 from azure.core.credentials import AzureKeyCredential
@@ -34,3 +40,16 @@ AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT")
 AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get(
     "AZURE_OPENAI_CHATGPT_DEPLOYMENT")
 AZURE_OPENAI_EMB_DEPLOYMENT = os.environ.get("AZURE_OPENAI_EMB_DEPLOYMENT")
+
+
+openai_client = AzureOpenAI(
+    api_key=AZURE_OPENAI_API_KEY,
+    api_version="2024-02-01",
+    azure_endpoint=AZURE_OPENAI_ENDPOINT
+)
+
+
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+# function which create title field and contents field's embeddings
+def generate_embeddings(text, model=AZURE_OPENAI_EMB_DEPLOYMENT):
+    return openai_client.embeddings.create(input=[text], model=model).data[0].embedding
