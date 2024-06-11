34f7980fa611de2023fa88099f4579b608dd898a
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-06-12 05:26:03 +0900

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # microsoft-ai-challenge-day
 This is the repository for Microsoft AI challenge day 2nd
+
+# How to run
+- install poetry
+- set up vertual environment
+```
+poetry install
+```
--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-import os
-from dotenv import load_dotenv
-import azure.search.documents
-azure.search.documents.__version__
-from azure.core.credentials import AzureKeyCredential
-from azure.search.documents import SearchClient, SearchIndexingBufferedSender
-from azure.search.documents.indexes import SearchIndexClient
-from azure.search.documents.models import (
-    QueryAnswerType,
-    QueryCaptionType,
-    QueryCaptionResult,
-    QueryAnswerResult,
-    SemanticErrorMode,
-    SemanticErrorReason,
-    SemanticSearchResultsType,
-    QueryType,
-    VectorizedQuery,
-    VectorQuery,
-    VectorFilterMode,
-)
+# This is the base file for RAG based on: 
+# ref: https://github.com/shohei1029/book-azureopenai-sample/blob/main/aoai-rag/notebooks/02_RAG_AzureAISearch_PythonSDK.ipynb
 
-load_dotenv()
-
-AZURE_SEARCH_ENDPOINT = os.environ.get("AZURE_SEARCH_ENDPOINT")
-AZURE_OPENAI_API_KEY= os.environ.get("AZURE_OPENAI_API_KEY")
-AZURE_OPENAI_SEARCH_INDEX = os.environ.get("AZURE_OPENAI_SEARCH_INDEX") #自動構築時のデフォルト設定
-
-credential = AzureKeyCredential(AZURE_OPENAI_API_KEY)
