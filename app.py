e06ccf3eb61f6701a7a3353e4a668f90ca842b10
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-06-12 07:57:32 +0900

    respond fixed konnitiha

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title("Yo-Ko-So: Sensyn Guide")
 prompt = st.chat_input("Please enter your question")
-print(prompt)
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        responce = "こんにちは"
+        st.markdown(responce)
